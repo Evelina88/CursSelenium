e7679c22aa89ab608a451c63d6623f5155864671
Evelina Chelaru <chelaru.evelina@gmail.com>
2022-09-23 20:58:55 +0300

    Curs 1

---
+++ src/main/java/Curs8/FirstSeleniumScript.java
@@ -0,0 +1,35 @@
+package Curs8;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstSeleniumScript {
+	
+	WebDriver driver;
+	
+	private static final String WebDriverMAnager = null;
+
+	@Test(priority = 1)
+	public void OpenBrowser() {
+		
+		driver = WebDriverManager.chromedriver().create();
+		driver.get("https://Keybooks.ro");
+		
+		
+		
+	}
+	@Test(priority = 2)
+	public void checkURl() {
+		String pageUrl = driver.getCurrentUrl();
+		System.out.println(pageUrl);
+		
+		assertEquals(pageUrl, "https://altex.ro");
+		
+		
+	}
+
+}
