8910434ea1b64176f9a4f79b79d4d2d2239654e9
Evelina Chelaru <chelaru.evelina@gmail.com>
2022-10-18 21:29:12 +0300

    curs15

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -18,21 +18,34 @@ public class LoginPage {
 	public By submitButton = By.className("submit_button");
 	
 	public By logoutButton = By.cssSelector("li[class='menu_user_logout']");
+    public By sucessLoginMessage = By.cssSelector("div[class*='sc_infobox_style_success']");
+    public By errorLoginMEssage = By.cssSelector("div[class*='sc_infobox_style_error']");
+	public By closePopUpButton = By.cssSelector("a[class='popup_close']");
 	
 	//metode
 	public void loginInApp(String user, String pass) {
 		Log.info("call method loginInApp");
+		driver.findElement(usernameField).clear();
 		driver.findElement(usernameField).sendKeys(user);
 		Log.info("send username" + user);
+		driver.findElement(passwordField).clear();
 		driver.findElement(passwordField).sendKeys(pass);
 		Log.info("sendpassword" + pass);
 		driver.findElement(submitButton).click();
 		Log.info("Clicked on submitted button");
+		
 	}
 	
 	public void logoutFromApp() {	
 	driver.findElement(logoutButton).click();
 	}
 	
-	
+	public boolean checkMEssageIsDisplayed(By locator) { 
+		
+		return driver.findElement(locator).isDisplayed();
+	}
+
+	public void closeLoginPop() {
+		driver.findElement(closePopUpButton).click();
+	}
 }
